9b21c92c6d44206f7908cf9e9a7a341bf4588c4e
rashid <raashidyaqoob@gmail.com>
2023-05-01 16:20:02 +0530

    Installing webpack

--- reactiverse/src/App.js
+++ reactiverse/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <p>Hi react test</p>
+        <div className="test">test</div>
+        <Header />
       </header>
     </div>
   );
