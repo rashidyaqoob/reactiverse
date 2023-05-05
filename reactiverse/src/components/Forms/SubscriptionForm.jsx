6a77b39d2193bb7c8fcbcebac4e8410b834b62f0
rashid <raashidyaqoob@gmail.com>
2023-05-05 16:30:33 +0530

    RCT-Subscribe_form: Implementation of functionality

--- reactiverse/src/components/Forms/SubscriptionForm.jsx
+++ reactiverse/src/components/Forms/SubscriptionForm.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-import Cookies from "js-cookie";
 import "./SubscriptionForm.scss";
-import { useCookies } from "react-cookie";
 
 const cookieArr = [];
 const Subscriptionform = () => {
@@ -9,84 +7,72 @@ const Subscriptionform = () => {
   const [status, setStatus] = useState("");
   //   const [cookie, setCookie] = useCookies(['email'])
 
-
-  const FORM_URL = "https://app.convertkit.com/forms/5120270/subscriptions";
-
-  const handleEmailCookie = (key, value) => {
-    cookieArr.push(key, value);
-  };
-  document.cookie = cookieArr;
+  // const FORM_URL = "https://app.convertkit.com/forms/5120270/subscriptions";
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
 
+  const handleCookie = (value) => {
+    cookieArr.push(email);
+    document.cookie = cookieArr;
+  };
+
+  const retrieveCookie = () => {
+    const cookie = document.cookie;
+    if (cookie.includes(email)) {
+      setStatus("success");
+    } else {
+      handleCookie(email);
+    }
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
+    retrieveCookie();
 
-    const data = new FormData(event.target);
+    // const data = new FormData(event.target);
 
-    try {
-      const response = await fetch(FORM_URL, {
-        method: "post",
-        body: data,
-        headers: {
-          accept: "application/json",
-        },
-      });
-      const json = await response.json();
-
-      if (json.status === "success") {
-        setStatus("success");
-        handleEmailCookie(`'email', ${email};`);
-      }
-    } catch (err) {
-      setStatus("failed");
-      console.log(err);
-    }
+    // try {
+    //   const response = await fetch(FORM_URL, {
+    //     method: "post",
+    //     body: data,
+    //     headers: {
+    //       accept: "application/json",
+    //     },
+    //   });
+    //   const json = await response.json();
+    //   console.log(json.status);
+    //   if (json.status == "success") {
+    //     console.log("hello");
+    //     setStatus("success");
+    //     handleEmailCookie(`'email', ${email};`);
+    //     console.log("success");
+    //   }
+    // } catch (err) {
+    //   setStatus("failed");
+    //   console.log(err);
+    // }
   };
 
   return (
     <>
-      {status == "success" && (
-        <>
-          <p>
-            Welcome aboard{email ? `, ${email}` : ""}{" "}
-            <span role="img" aria-label="Ship">
-              🚢
-            </span>
-          </p>
-          <p>Please check your inbox to confirm the subscription!</p>
-        </>
-      )}
-      {status === "failed" && (
-        <>
-          <p>Oops, something went wrong...</p>
-          <p>
-            Please, <button onClick={() => setStatus(null)}>try again.</button>
-          </p>
-        </>
-      )}
-
-      {/* {status == "" && ( */}
-      <>
-        <h2 class="form-title">Subscribe</h2>
-        <form className="subscribe-form" action="" onSubmit={handleSubmit}>
-          <input
-            type="email"
-            placeholder="Email"
-            name="email_address"
-            class="subscribe-form__email"
-            onChange={handleEmailChange}
-            value={email}
-          />
-          <button type="submit" class="subscribe-form__btn">
-            Subscribe
-          </button>
-          {/* {cookies.email} */}
-        </form>
-      </>
-      {/* )} */}
+      <h2 class="form-title">Subscribe</h2>
+      <form className="subscribe-form" action="" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          name="email_address"
+          class="subscribe-form__email"
+          onChange={handleEmailChange}
+          value={email}
+        />
+        <button type="submit" class="subscribe-form__btn">
+          Subscribe
+        </button>
+        {status == "success" ? <p>Already Subscribed</p> : ""}
+        {/* {cookies.email} */}
+      </form>
     </>
   );
 };
