a0779115717f3224e95932d4334aa359b37f8b2b
rashid <raashidyaqoob@gmail.com>
2023-05-10 18:56:48 +0530

    RCT-improve: Refactor

--- reactiverse/src/App.js
+++ reactiverse/src/App.js
@@ -3,7 +3,6 @@ import Header from "./components/header/header.jsx";
 import About from "./pages/About.jsx";
 import ContactUs from "./pages/Contact-us.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import HandleSignInBtn from "./components/Signup/Signup.jsx";
 import SignUp from "./pages/SignUp/Sign-up.jsx";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <HandleSignInBtn />
         <Header />
         <Routes>
           <Route path="/" />
