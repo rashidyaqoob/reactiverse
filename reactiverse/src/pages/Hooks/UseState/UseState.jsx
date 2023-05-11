ca92431ea837c301101422615730e61aa449a03a
Tehreem Fazili <tehreemfazili5@gmail.com>
2023-05-11 14:03:03 +0530

    RCT-hooks: Added usestate example for strings

--- reactiverse/src/pages/Hooks/UseState/UseState.jsx
+++ reactiverse/src/pages/Hooks/UseState/UseState.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import "./UseState.scss";
 
 const UseState = () => {
   const [count, setCount] = useState(0);
+  const [name1, setName1] = useState("");
+  const [name2, setName2] = useState("");
+  const [updatedName, setUpdateName] = useState('');
+  const [arrayList, setArrayList] = useState([]);
+  const [arrayItem, setArrayItem] = useState('');
+
 
   function handleDecrement() {
     setCount((count) => count - 1);
@@ -10,6 +17,26 @@ const UseState = () => {
 
   function handleIncrement() {
     setCount((count) => count + 1);
+  }
+
+  function handleInputSubmit() {
+    setUpdateName(name2)
+  }
+    // useEffect(()=> {
+    //     setArray(array => [...array, arrayItem])
+    // },[arrayItem])
+
+  function handleItemChange (event) {
+    setArrayItem(event.target.value)
+  }
+
+  function addItemToArray(event) {
+    event.preventDefault();
+    console.log("Array Item: ", arrayItem)
+    setArrayList([...arrayList, arrayItem])
+    setArrayItem('')
+    console.log(arrayList)
+
   }
 
   return (
@@ -22,7 +49,7 @@ const UseState = () => {
         </p>
       </div>
       <div className="hook__examples">
-        <div>
+        <div className="hook__examples__int">
           <h2>Counter</h2>
           <button className=" btn btn-decreament" onClick={handleDecrement}>
             {"-"}
@@ -31,6 +58,25 @@ const UseState = () => {
           <button className=" btn btn-increament" onClick={handleIncrement}>
             {"+"}
           </button>
+        </div>
+        <div className="hooks__example__string">
+            <h2>Enter Name</h2>
+            <input type='text' className="input-name" placeholder="Enter Name" onChange={(event)=> setName1(event.target.value)} value={name1} />
+            <p>Set Name: {name1}</p>
+        </div>
+        <div className="hooks__example__string">
+            <h2>Enter Name</h2>
+            <input type='text' className="input-name" placeholder="Enter Name" onChange={(event)=> setName2(event.target.value)} value={name2} />
+            <p>Set Name: {name2}</p>
+            <p>Update Name: {updatedName}</p>
+            <button className="btn btn-enter" onClick={handleInputSubmit}>Enter</button>
+        </div>
+        <div className="hook__example__array">
+            <input placeholder="Enter New Item" type='text' value={arrayItem} onChange={handleItemChange}  />
+            <button onClick={addItemToArray}>Click</button>
+            {   arrayList.map((item)=> {
+                <div>{item}</div>
+            })}
         </div>
       </div>
     </div>
