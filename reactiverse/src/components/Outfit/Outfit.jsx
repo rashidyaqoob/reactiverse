fa6135d261e1a3477a224ba93685bdf116a8293f
rashid <raashidyaqoob@gmail.com>
2023-05-31 13:18:11 +0530

    RCT-outfits: Implementation of multiple filters

--- reactiverse/src/components/Outfit/Outfit.jsx
+++ reactiverse/src/components/Outfit/Outfit.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable array-callback-return */
 import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import { BASE_URL } from "../../utils/base-url/BASE_URL";
 import Select from "react-select";
 import Outfitcard from "./Outfit-card";
@@ -7,11 +9,18 @@ const Outfit = () => {
   const [men, setMen] = useState([]);
   const [women, setWomen] = useState([]);
   const [filterValue, setFiltervalue] = useState("");
-  console.log(filterValue);
-  const options = [
+  const [filterBrand, setBrandvalue] = useState("");
+  const location = useLocation();
+  const navigate = useNavigate();
+  const optionsGender = [
     { value: "", label: "All" },
     { value: "men", label: "Men" },
     { value: "women", label: "Women" },
+  ];
+  const optionsBrand = [
+    { value: "", label: "All" },
+    { value: "puma", label: "Puma" },
+    { value: "nike", label: "Nike" },
   ];
 
   useEffect(() => {
@@ -24,13 +33,54 @@ const Outfit = () => {
       .catch((error) => console.error("Error fetching JSON data:", error));
   }, []);
 
+  function handleFilter(opt) {
+    const searchParams = new URLSearchParams(location.search);
+    searchParams.set("gender", opt.value);
+
+    navigate({
+      search: `?${searchParams.toString()}`,
+    });
+    setFiltervalue(opt.value);
+    console.log(filterValue);
+  }
+
+  function handleFilterBrand(brand) {
+    const searchParams = new URLSearchParams(location.search);
+    searchParams.set("brand", brand.value);
+
+    navigate({
+      search: `?${searchParams.toString()}`,
+    });
+    setBrandvalue(brand.value);
+  }
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    console.log(searchParams);
+    if (!searchParams.has("option")) {
+      const newSearchParams = new URLSearchParams();
+      window.history.replaceState(
+        {},
+        "",
+        `${location.pathname}${newSearchParams.toString()}`
+      );
+    }
+  }, []);
+
   return (
     <div className="outfits-container">
       <div className="outfit-dropdown">
-        <label for="gender">Filter</label>
+        <label for="gender">Filter Gender</label>
         <Select
-          options={options}
-          onChange={(opt) => setFiltervalue(opt.value)}
+          options={optionsGender}
+          touchUi={false}
+          onChange={handleFilter}
+        />
+        <label for="gender">Filter Brand</label>
+        <Select
+          options={optionsBrand}
+          touchUi={false}
+          onChange={handleFilterBrand}
         />
       </div>
       <h2>OUTFITS</h2>
@@ -39,18 +89,22 @@ const Outfit = () => {
           <>
             <h3>Men</h3>
             <ul className="outfits-jeans">
-              {men.map((item, key) => (
-                <li id={key} className="outfit-list-item">
-                  <Outfitcard
-                    outfit_brand={item.brand}
-                    outfit_rate={item.rate}
-                    outfit_size={item.size}
-                    outfit_color={item.color}
-                    outfit_offer={item.offer}
-                    outfit_image={item.image}
-                  />
-                </li>
-              ))}
+              {men
+                .filter(
+                  (item) => item.brand === filterBrand || filterBrand === ""
+                )
+                .map((item, key) => (
+                  <li id={key} className="outfit-list-item">
+                    <Outfitcard
+                      outfit_brand={item.brand}
+                      outfit_rate={item.rate}
+                      outfit_size={item.size}
+                      outfit_color={item.color}
+                      outfit_offer={item.offer}
+                      outfit_image={item.image}
+                    />
+                  </li>
+                ))}
             </ul>
           </>
         )}
@@ -58,18 +112,22 @@ const Outfit = () => {
           <>
             <h3>Women</h3>
             <ul className="outfits-jackets">
-              {women.map((item, key) => (
-                <li id={key} className="outfit-list-item">
-                  <Outfitcard
-                    outfit_brand={item.brand}
-                    outfit_rate={item.rate}
-                    outfit_size={item.size}
-                    outfit_color={item.color}
-                    outfit_offer={item.offer}
-                    outfit_image={item.image}
-                  />
-                </li>
-              ))}
+              {women
+                .filter(
+                  (item) => item.brand === filterBrand || filterBrand === ""
+                )
+                .map((item, key) => (
+                  <li id={key} className="outfit-list-item">
+                    <Outfitcard
+                      outfit_brand={item.brand}
+                      outfit_rate={item.rate}
+                      outfit_size={item.size}
+                      outfit_color={item.color}
+                      outfit_offer={item.offer}
+                      outfit_image={item.image}
+                    />
+                  </li>
+                ))}
             </ul>
           </>
         )}
