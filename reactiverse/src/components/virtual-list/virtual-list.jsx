ab0e757522948b41def002a67d13cb8a57ef0306
Tehreem Fazili <tehreemfazili5@gmail.com>
2023-07-10 11:01:41 +0530

    RCT-list: implemeted grid and collection

--- reactiverse/src/components/virtual-list/virtual-list.jsx
+++ reactiverse/src/components/virtual-list/virtual-list.jsx
@@ -6,12 +6,19 @@ import {
   CellMeasurer,
   CellMeasurerCache,
   ScrollSync,
+  Grid,
+  Collection,
 } from "react-virtualized";
 import "./virtual-list.scss";
 
+const columnCount = 100;
 const rowCount = 500;
+const columnWidth = 400;
 const listHeight = 500;
 const rowHeight = 50;
+const cellCount = 5000;
+const cellWidth = 400;
+const cellHeight = 50;
 const rowWidth = 500;
 const cache = new CellMeasurerCache({
   fixedWidth: true,
@@ -35,6 +42,19 @@ const list = Array(rowCount)
       }),
     };
   });
+
+const grid = Array(rowCount).fill().map((val, idx) => (
+  Array(columnCount).fill().map((val, idx) => ({
+    id: idx,
+    name: 'John Doe',
+    image: 'http://via.placeholder.com/40',
+    text: loremIpsum({
+      count: 4,
+      units: 'word'
+    })
+  }))
+));
+
 
 function Collapsible({ children, title, onChange }) {
   const [expanded, setExpanded] = useState(false);
@@ -77,6 +97,43 @@ function renderRow({ index, key, style, parent }) {
   );
 }
 
+function renderCellCollection({ index, key, style }) {
+  return (
+    <div key={key} style={style} className="row">
+      <div className="image">
+        <img src={list[index].image} alt="" />
+      </div>
+      <div className="content">
+        <div>{list[index].name}</div>
+        <div>{list[index].text}</div>
+      </div>
+    </div>
+  );
+}
+
+function renderCell({ columnIndex, key, rowIndex, style }) {
+  return (
+    <div key={key} style={style} className="row">
+      <div className="image">
+        <img src={grid[rowIndex][columnIndex].image} alt="" />
+      </div>
+      <div className="content">
+        <div>{grid[rowIndex][columnIndex].name}</div>
+        <div>{grid[rowIndex][columnIndex].text}</div>
+      </div>
+    </div>
+  );
+}
+
+function cellSizeAndPositionGetter({ index }) {
+  return {
+    height: cellHeight,
+    width: cellWidth,
+    y: index * cellHeight,
+    x: Math.floor(Math.random() * 10) * cellWidth
+  }
+}
+
 function VirtualList() {
   return (
     <>
@@ -86,20 +143,37 @@ function VirtualList() {
             {({ onScroll, scrollTop, scrollLeft }) => (
               <div className="list">
                 <span>Scroll Top - {scrollTop}</span>
+                <span>Scroll Left - {scrollLeft}</span>
                 <AutoSizer>
                   {({ width, height }) => {
                     return (
-                      <List
-                        width={width}
-                        height={height}
-                        deferredMeasurementCache={cache}
-                        rowHeight={cache.rowHeight}
-                        rowRenderer={renderRow}
-                        onScroll={onScroll}
-                        rowCount={list.length}
-                        overscanRowCount={3}
-                      />
-                    );
+                      // <List
+                      //   width={width}
+                      //   height={height}
+                      //   deferredMeasurementCache={cache}
+                      //   rowHeight={cache.rowHeight}
+                      //   rowRenderer={renderRow}
+                      //   onScroll={onScroll}
+                      //   rowCount={list.length}
+                      //   overscanRowCount={3}
+                      // />
+                      // <Grid
+                      //   width={width}
+                      //   height={height}
+                      //   rowHeight={rowHeight}
+                      //   columnWidth={columnWidth}
+                      //   cellRenderer={renderCell}
+                      //   onScroll={onScroll}
+                      //   rowCount={grid.length}
+                      //   columnCount={grid[0].length}/>
+                    // );
+                    <Collection
+                      width={width}
+                      height={height}
+                      cellRenderer={renderCellCollection}
+                      cellCount={list.length}
+                      cellSizeAndPositionGetter={cellSizeAndPositionGetter}/>
+                    )
                   }}
                 </AutoSizer>
               </div>
