a73ac4483530ffb8c5067c73fd9b2cb702bc7947
Rashid <rashid.yaqoob@axelerant.com>
2024-02-29 15:58:21 +0530

    RCT-blogs--submit-functionality: Enable DB to fetch posts

--- reactiverse/src/pages/Blogs/Blogs.jsx
+++ reactiverse/src/pages/Blogs/Blogs.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react'
 import BlogsHeader from './BlogsHeader'
 import BlogPost from './BlogPost'
-import BlogNewPost from './BlogNewPost'
+import BlogNewPost from './BlogNewPost';
+import axios from 'axios';
 
 const Blogs = () => {
   const [isModel, setIsModel] = useState(false);
@@ -21,7 +23,7 @@ const Blogs = () => {
   function closeModel() {
     setIsModel(false);
   }
-  function submitHandler(e) {
+  async function submitHandler(e) {
     e.preventDefault();
     closeModel();
 
@@ -30,14 +32,29 @@ const Blogs = () => {
       body: body,
     };
     setPosts([newPost, ...posts]); 
+    try {
+      await axios.post('http://localhost:8000/posts', newPost);
+    }
+    catch(err) {
+      console.log(err)
+    }
+
   }
+  useEffect(()=> {
+    async function fetchPosts() {
+      const res = await axios.get('http://localhost:8000/posts', {author, body})
+      const existingPosts = await res.data;
+      setPosts(existingPosts, ...posts)
+    }
+    fetchPosts();
+  },[])
   return (
     <div style={{ maxWidth: "1190px", margin: '70px auto' }}>
       <BlogsHeader modelHandler={openModel}/>
       <BlogNewPost isModel= {isModel} modelHandler={closeModel} authorEntered={authorEntered} bodyEntered={bodyEntered} submitHandler={submitHandler}/>
       <div style={{display: 'flex', flexWrap: 'wrap'}}>
         {posts.length === 0 && (<div style={{marginTop: '32px'}}>No posts yet. Please add new posts</div>) }
-        {posts.map(post=> <BlogPost author={post.author} body={post.body}/> )}
+        {posts.map(post=> <BlogPost key={post.author} author={post.author} body={post.body}/> )}
       </div>
     </div>
   )
