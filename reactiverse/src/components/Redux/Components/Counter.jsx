8c900d73d09db19509e699f38688039f12db24c2
Tehreem Fazili <tehreemfazili5@gmail.com>
2024-04-29 10:34:07 +0530

    RCT-redux: redux count app using connect function

--- reactiverse/src/components/Redux/Components/Counter.jsx
+++ reactiverse/src/components/Redux/Components/Counter.jsx
@@ -1,33 +1,48 @@
 import React from "react";
 import { useDispatch, connect, useSelector } from "react-redux";
 
-const Counter = () => {
-  const dispatch = useDispatch();
-  const counter = useSelector(state =>  state.counter);
+// const Counter = () => {
+//   const dispatch = useDispatch();
+//   const counter = useSelector(state =>  state.counter);
+//   return (
+//     <div>
+//       <p className="counter_title">Counter: {counter}</p>
+//       <button className="button" onClick={() => dispatch({ type: "INCREMENT" })}>
+//         Increment
+//       </button>
+//       <button className="button" onClick={() => dispatch({ type: "DECREMENT" })}>
+//         Decrement
+//       </button>
+//     </div>
+//   );
+// };
+
+
+const Counter = ({ counter, increment, decrement }) => {
   return (
     <div>
       <p className="counter_title">Counter: {counter}</p>
-      <button className="button" onClick={() => dispatch({ type: "INCREMENT" })}>
+      <button className="button" onClick={increment}>
         Increment
       </button>
-      <button className="button" onClick={() => dispatch({ type: "DECREMENT" })}>
+      <button className="button" onClick={decrement}>
         Decrement
       </button>
     </div>
   );
 };
 
-// const mapStateToProps = (state) => ({
-//   counter: state.counter 
+const mapStateToProps = (state) => ({
+  counter: state.counter 
   //  Use 'counter: state.counter.counter' and replace the above line if you are using combineReducers to ensure that 'counter' matches the correct key in your store.
-// });
+});
 
-// const mapDispatchToProps = (dispatch) => (
-//   {
-    
-  
-//   increment: () => dispatch({ type: "INCREMENT" }),
-//   decrement: () => dispatch({ type: "DECREMENT" })
-// });
+const mapDispatchToProps = (dispatch) => (
+{
+  increment: () => dispatch({ type: "INCREMENT" }),
+  decrement: () => dispatch({ type: "DECREMENT" })
+});
 
-export default Counter;
+// export default Counter;
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+
--- reactiverse/src/components/Redux/Redux.jsx
+++ reactiverse/src/components/Redux/Redux.jsx
@@ -2,12 +2,23 @@ const initialState = {
   counter: 0
 };
 
-const counterReducer = (state = 0, action) => {
+// const counterReducer = (state = 0, action) => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// };
+
+const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return state + 1;
+      return { ...state, counter: state.counter + 1 };
     case "DECREMENT":
-      return state - 1;
+      return { ...state, counter: state.counter - 1 };
     default:
       return state;
   }
